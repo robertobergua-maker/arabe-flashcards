589bf8de125c8cadfbb4bb2a6af9a0b568a99af5
Roberto <roberto.bergua@gmail.com>
2026-02-08 22:19:03 +0100

    as

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ErrorBoundary } from "./ErrorBoundary";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
